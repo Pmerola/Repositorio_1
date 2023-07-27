4f5298c5ed56981e597ec5e7eeabaa2a7815dd00
Pablo Merola <merolapablo089@gmail.com>
2023-07-27 16:30:28 -0300

    Agrego validacion de mayor de edad

--- Ejercicios_Bucles_Python/Ejercicio_1_c.py
+++ Ejercicios_Bucles_Python/Ejercicio_1_c.py
@@ -2,3 +2,8 @@
 #Escribir un programa que pregunte al usuario su edad y muestre por pantalla si es mayor de edad o no.
 
 edad_ingresada = int(input("Ingrese su edad: "))
+
+if edad_ingresada > 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
