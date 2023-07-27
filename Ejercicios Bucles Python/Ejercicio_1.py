5a56f081ee5139b7c40b5162e7a64dcfddbb66b5
Pablo Merola <merolapablo089@gmail.com>
2023-07-27 15:03:00 -0300

    Elimino archivos para volverlos a subir individualemente

--- Ejercicios Bucles Python/Ejercicio_1.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 1
-#Escribir un programa que pida al usuario una palabra y la muestre por pantalla 10 veces.
-
-nombre = input("Ingrese su nombre: ")
-
-for nombres in range (11):
-    print(nombre)
-
--- Ejercicios Bucles Python/Ejercicio_4.py
+++
@@ -1,8 +0,0 @@
-#Ejercicio 4
-#Escribir un programa que pida al usuario un número entero positivo 
-#y muestre por pantalla la cuenta atrás desde ese número hasta cero 
- 
-valor = int(input("Ingrese un valor: "))
-
-for i in range(valor, -1 ,-1):
-    print(i)
