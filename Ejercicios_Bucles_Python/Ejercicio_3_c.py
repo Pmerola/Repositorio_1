f0a9ae04107f3887b37fa936f1f1dc3008b0f077
Pablo Merola <merolapablo089@gmail.com>
2023-07-28 16:36:16 -0300

    Realizo ejercicio 3 de condicional

---
+++ Ejercicios_Bucles_Python/Ejercicio_3_c.py
@@ -0,0 +1,10 @@
+#Escribir un programa que pida al usuario dos números y muestre por pantalla su división.
+# Si el divisor es cero el programa debe mostrar un error.
+
+numero = int(input("Ingrese un numero: "))
+divisor = int(input("Ingrese el numero divisor: "))
+
+if divisor == 0:
+    print("ERROR, el divisor no puede ser 0")
+resultado = numero / divisor
+print("El resultado de su division es:", resultado)
