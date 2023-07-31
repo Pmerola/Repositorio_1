dd97bd612b411a9d989bf8c03d6c826199a214be
Pablo Merola <merolapablo089@gmail.com>
2023-07-31 17:17:29 -0300

    Realizo Ejercicio 1.1 de bucles, REVISAR la logica (sistema de repeticion de valores ingresados)

---
+++ Ejercicios_Bucles_Python/Ejercicio_1.1_b.py
@@ -0,0 +1,10 @@
+#Ejercicio 1.1
+#Hacer el ejercicio 1 pero usando otra instrucción iterativa.
+#decir, si en el 1 usaste un while, hacerlo con un for o al revés.
+
+palabra = input("Ingrese una palabra: ")
+
+contador = 0
+while contador == 10:
+    print(palabra)
+    contador = +1
