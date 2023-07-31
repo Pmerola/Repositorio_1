6283eba8f08cc0adb47d5ff220558b7e1c66fbe7
Pablo Merola <merolapablo089@gmail.com>
2023-07-31 17:34:22 -0300

    Realizo ejercicio 2 de bucles (Sistema de contador de edad)

---
+++ Ejercicios_Bucles_Python/Ejercicio_2_b.py
@@ -0,0 +1,8 @@
+#Ejercicio 2
+#Escribir un programa que pregunte al usuario su edad
+#y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+
+edad_ingresada = int(input("Ingrese su edad: "))
+
+for i in range(1,edad_ingresada):
+    print(i)
