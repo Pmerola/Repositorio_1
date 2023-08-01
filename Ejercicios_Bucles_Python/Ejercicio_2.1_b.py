0acece070ff97bddbddc18acc6d5b65fa287a598
Pablo Merola <merolapablo089@gmail.com>
2023-07-31 23:14:52 -0300

    Realizo ejercicio 2.1 de bucles, (Sistema en el que se le muestra al usuario dependiendo la edad que ingrese, cuantos años previo a su edad hay)

---
+++ Ejercicios_Bucles_Python/Ejercicio_2.1_b.py
@@ -0,0 +1,14 @@
+##Ejercicio 2.1
+#Hacer el ejercicio 2 pero usando otra instrucción iterativa.
+#Es decir, si en el 2 usaste un while, hacerlo con un for o al revés.
+#Escribir un programa que pregunte al usuario su edad
+#y muestre por pantalla todos los años que ha cumplido (desde 1 hasta su edad).
+
+edad = int(input("Ingrese su edad: "))
+
+acumulador = 1
+
+while acumulador < edad:
+    print(acumulador)
+    acumulador += 1
+
