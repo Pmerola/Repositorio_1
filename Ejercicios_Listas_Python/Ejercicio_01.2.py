e0b71190047897c3a68995947051143dc34eae24
Pmerola <merolapablo089@gmail.com>
2023-08-24 07:31:10 -0300

    Realizo ejercicio 1.2

---
+++ Ejercicios_Listas_Python/Ejercicio_01.2.py
@@ -0,0 +1,7 @@
+#Ejercicio 1.2
+#Escribir un programa que almacene las asignaturas de un curso 
+#(por ejemplo Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por pantalla. 
+#Sin hardcodear la lista de asignaturas.
+
+asignatura = [input("Ingrese,las asignaturas que desee ingresar: ")]
+print(asignatura)
